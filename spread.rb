cfafec3b7b0d56462d8fc0def762162de8488836
Tim Fogarty <timothy.fogo@gmail.com>
2018-02-26 02:21:44 -0800

    Update SHA for v2.0.0

--- spread.rb
+++ spread.rb
@@ -2,7 +2,7 @@ class Spread < Formula
   desc "Run scripts and commands in multiple directories at once"
   homepage "https://github.com/tfogo/spread"
   url "https://github.com/tfogo/spread/releases/download/v2.0.0/spread-2.0.0.tar.gz"
-  sha256 "c47d5ff74ad7ecbaef852607fbf570069ec4611dcaed1d281337f5d0c72644e5"
+  sha256 "df03858d09ef7b74ce0c0b0b656247ad5908c6750cd25c1d859dc2df86391889"
 
   bottle :unneeded
 
