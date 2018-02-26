e5567718bc7029655c71dee4cf9613953995cb9f
Tim Fogarty <timothy.fogo@gmail.com>
2018-02-26 02:16:45 -0800

    Update spread to v2.0.0

--- spread.rb
+++ spread.rb
@@ -1,7 +1,7 @@
 class Spread < Formula
   desc "Run scripts and commands in multiple directories at once"
   homepage "https://github.com/tfogo/spread"
-  url "https://github.com/tfogo/spread/releases/download/v1.0.1/spread-1.0.1.tar.gz"
+  url "https://github.com/tfogo/spread/releases/download/v2.0.0/spread-2.0.0.tar.gz"
   sha256 "c47d5ff74ad7ecbaef852607fbf570069ec4611dcaed1d281337f5d0c72644e5"
 
   bottle :unneeded
